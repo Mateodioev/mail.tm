6c050977e6135b918fcbed592e6bbe88c4ee042a
Mateodioev <68271130+Mateodioev@users.noreply.github.com>
2022-01-04 10:36:17 -0600

    The send function was improved
    Along with this all the code was improved

--- mailTm.php
+++ mailTm.php
@@ -1,208 +1,184 @@
 <?php
+
+
 /**
  * PHP class for use mail.tm api
- * Documentation: https://api.mail.tm/
- * Github repository: https://github.com/Mateodioev/mail.tm
- * @author: @Mateodioev
- * Telegram Channel: https://t.me/caspercardergil
+ * @api-link: https://api.mail.tm/
+ * @link https://github.com/Mateodioev/mail.tm
+ * @author Mateodioev
  */
-
 class MailTm {
-	const VERSION = '0.1';
 
-	private static array $cURL = [
-		CURLOPT_RETURNTRANSFER => true,
-        	CURLOPT_HEADER         => false,
-        	CURLINFO_HEADER_OUT    => true,
-        	CURLOPT_FOLLOWLOCATION => true,
-        	CURLOPT_AUTOREFERER    => true,
-        	CURLOPT_CONNECTTIMEOUT => 30,
-        	CURLOPT_TIMEOUT        => 60,
-        	CURLOPT_SSL_VERIFYPEER => false,
-        	CURLOPT_SSL_VERIFYHOST => 0
-	];
+    private static string $baseUrl = 'https://api.mail.tm/';
+    private static string $id      = "";
+    private static string $token   = "";
 
-	private static string $jwtToken  = '';
-	private static string $accountId = '';
-	private static string $msgId     = '';
-	private static string $mail      = '';
-	private static string $password  = '';
-	private static string $domain    = '';
+    public static string $mail     = "";
+    public static string $password = "";
 
-	private static array $messages;
 
-	private static $ch;
-	private static $response;
-	private static $info;
-	private static $error_string;
+    /**
+     * Interactuar con la API
+     */
+    private static function send(string $path, string $method = 'GET', $body = null) : array {
+        
+        $headers = [
+            'accept: application/json',
+            'authorization: Bearer ' . self::$token
+        ];
 
-	/**
-	 * Function to send resquest to api
-	 */
-	private static function req($url, $headers = NULL, $method = 'GET', $post = NULL) {
-		self::$ch = curl_init($url);
-		curl_setopt_array(self::$ch, self::$cURL);
-		curl_setopt_array(self::$ch, [CURLOPT_CUSTOMREQUEST => $method]);
+        if (in_array($method, ['POST', 'PATCH'])) {
+            $contentType = $method == 'PATCH' ? 'merge-patch+json' : 'json';
+            $headers[] = 'content-type: application/'.$contentType;
+            $body = json_encode($body);
+        }
+        
+        $ch = curl_init(self::$baseUrl . $path);
+        curl_setopt_array($ch, [
+            CURLOPT_CUSTOMREQUEST  => $method,
+            CURLOPT_HTTPHEADER     => $headers,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_SSL_VERIFYPEER => false,
+            CURLOPT_SSL_VERIFYHOST => 0
+        ]);
 
-		// Set headers, when $headers var is array
-		if (!empty($headers) && is_array($headers)) {
-			curl_setopt_array(self::$ch, [CURLOPT_HTTPHEADER => $headers]);
-		}
-		// Put data in postfields
-		if ($post && $post != NULL) {
-			curl_setopt_array(self::$ch, [CURLOPT_POST => true, CURLOPT_POSTFIELDS => $post]);
-		}
+        if ($body != null) curl_setopt_array($ch, [CURLOPT_POST => true, CURLOPT_POSTFIELDS => $body]);
 
-		// RUN resquest 
-		self::$response = curl_exec(self::$ch);
-		self::$info     = curl_getinfo(self::$ch);
+        $response = curl_exec($ch);
+        $info = curl_getinfo($ch);
 
-		if (self::$response === FALSE) {
+        if ($info['http_code'] >= 400) {
+            $error = curl_error($ch);
+            curl_close($ch);
+            return [
+                'ok' => false,
+                'code' => $info['http_code'],
+                'info' => $info,
+                'error' => $error,
+                'response' => $response
+            ];
+        } else {
+            curl_close($ch);
+            return [
+                'ok' => true,
+                'code' => $info['http_code'],
+                'body' => json_decode($response, true)
+            ];
+        }
+    }
 
-			self::$error_string = curl_error(self::$ch);
+    /**
+     * Crea datos random para el email y password
+     */
+    private static function CreateData(): void {
+        self::$mail = uniqid();
+        self::$password = str_shuffle(substr(uniqid('12$@3456'), 0, 15));
+    }
 
-			// Close the resquest
-			curl_close(self::$ch);
+    /**
+     * Retrieves a Domain resource.
+     */
+    public static function GetDomains() : array 
+    {
+        $datas = self::send('domains?page=1');
 
-			return [
-				'success' => false,
-				'code'    => self::$info['http_code'],
-				'error'   => self::$error_string,
-			];
-		} else {
-			// Close the resquest
-			curl_close(self::$ch);
+        if ($datas['code'] == 200) {
+            return ['ok' => true, 'domains' => $datas['body'][0]['domain']];
+        } else {
+            return ['ok' => false, 'error' => $datas['error']];
+            
+        }
+        
+    }
 
-			return [
-				'success' => true,
-				'code'    => self::$info['http_code'],
-				'headers' => self::$info,
-				'body'    => self::$response,
-			];
-		}
-	}
+    /**
+     * Creates a Account resource.
+     */
+    public static function CreateAccount(?string $prefix = null, ?string $pass = null) : array 
+    {
+        self::CreateData();
 
-	// Set an password and mail for the resquest
-	private static function Datas() {
-		self::$mail = uniqid();
-		self::$password = str_shuffle(uniqid('1$@'));
-	}
+        self::$mail = $prefix ?? self::$mail;
+        self::$password = $pass ?? self::$password;
 
-	// Get domains avaliable 
-	private static function GetDomains() {
-		// send resquest to the api
-		$data = self::req('https://api.mail.tm/domains?page=1')['body'];
-		$datas = json_decode($data, true);
-		// Decode info
-		self::$domain = $datas['hydra:member'][0]['domain'];
-	}
+        $domains = self::GetDomains();
 
-	// Create the account | establece el accountId
-	public static function CreateAccount($mailI = NULL, $passI = NULL) {
-		if (empty($mailI) || empty($passI)) {
-			// Create the acount with random datas
-			self::Datas();
-		} else {
-			self::$mail = $mailI;
-			self::$password = $passI;
-		}
-		self::GetDomains();
-		$mailn = self::$mail.'@'.self::$domain;
-		self::$mail = $mailn;
+        if (!$domains['ok']) return $domains;
 
-		$header = ['Accept: application/ld+json', 'Content-Type: application/ld+json'];
-		$post = json_encode([
-			'address' => self::$mail,
-			'password' => self::$password,
-		]);
-		$data = self::req('https://api.mail.tm/accounts', $header, 'POST', $post)['body'];
-		$datas = json_decode($data, true);
-		self::$accountId = $datas['id'];
-		return [
-			'mail' => self::$mail,
-			'pass' => self::$password,
-			'accid' => self::$accountId
-		];
-	}
+        self::$mail = self::$mail . '@' . $domains['domains'];
 
-	// GET JWT token
-	public static function JwtToken($mails = NULL, $pass = NULL) {
-		if (empty(self::$mail)) {
-			// Function CreateAccount was no called
-			$post = json_encode(['address' => $mails, 'password' => $pass]);
-		} else {
-			$post = json_encode(['address' => self::$mail, 'password' => self::$password]);
-		}
+        $data = self::send('accounts', 'POST', ['address' => self::$mail, 'password' => self::$password]);
+        
+        if (!$data['ok']) return $data;
 
-		$header = ['Accept: application/json', 'Content-Type: application/json'];
-		$data = self::req('https://api.mail.tm/token', $header, 'POST', $post)['body'];
-		$datas = json_decode($data, true);
+        $datas = $data['body'];
+        self::$id = $datas['id'];
+        return ['ok' => true, 'mail' => self::$mail, 'pass' => self::$password, 'accid' => self::$id];
+    }
 
-		self::$jwtToken = $datas['token'];
-		return self::$jwtToken;
-	}
+    /**
+     * Get jwt token to login
+     */
+    public static function GetToken(?string $mail = null, ?string $pass = null) : array 
+    {
+        $mail = $mail ?? self::$mail;
+        $pass = $pass ?? self::$password;
+        $post = ['address' => $mail, 'password' => $pass];
 
-	// DETELE ACCOUNT
-	public static function DeleteAccount($jwtTokens = NULL, $accId = NULL) {
-		if (empty(self::$jwtToken)) {
-			// Function JwtToken was no called
-			$headers = ['Accept: */*', 'Authorization: Bearer ' . $jwtTokens];
-		} else {
-			$headers = ['Accept: */*', 'Authorization: Bearer ' . self::$jwtToken];
-		}
+        $data = self::send('token', 'POST', $post);
+        if (!$data['ok']) return $data;
 
-		if (!empty(self::$accountId)) {
-			$accId = self::$accountId;
-		}
-		$a = self::req('https://api.mail.tm/accounts/'.$accId, $headers, 'DELETE');
-		return json_decode($a['body'], true);
-	}
+        self::$token = $data['body']['token'];
+        return ['ok' => true, 'token' => $data['body']['token'], 'id' => $data['body']['id']];
+    }
 
-	// GET MSGS
-	public static function GetMessage($token = NULL) {
-		if (!empty(self::$jwtToken)) {
-			// Function JwtToken was called
-			$token = self::$jwtToken;
-		}
-		$headers = ['Accept: application/ld+json', 'Authorization: Bearer '.$token];
-		// Send resquest
-		$data = self::req('https://api.mail.tm/messages?page=1', $headers, 'GET')['body'];
-		$datas = json_decode($data, true);
-		if (isset($datas['code'])) {
-			// Invalid JWT token
-			return ['success' => false, 'msg' => '['.$datas['code'].'] '.$datas['message']];
-		} else {
-			self::$messages = $datas['hydra:member'];
-			return [
-				'success' => true,
-				'messages' => self::$messages,
-				'total' => (int) $datas['hydra:totalItems']
-			];
-		}
-	}
+    /**
+     * Removes the Account resource.
+     */
+    public static function Delete(?string $token = null, ?string $accid = null) : array 
+    {
+        $accid = self::$id ?? $accid;
+        self::$token = $token ?? self::$token;
 
-	// Get msg id
-	public static function GetMessageId($msgID, $token = NULL) {
-		if (!empty(self::$jwtToken)) {
-			// Function JwtToken was called
-			$token = self::$jwtToken;
-		}
-		$headers = ['Authorization: Bearer ' . $token];
-		// Send resquest
-		$data = self::req('https://api.mail.tm/messages/'.$msgID, $headers, 'GET')['body'];
-		$datas = json_decode($data, true);
-		if (isset($datas['hydra:description'])) {
-			// Msg id no found
-			return ['success' => false, 'msg' => $datas['hydra:title']."\n".$datas['hydra:description'] ];
-		} elseif (isset($datas['code'])) {
-			// Invalid JWT TOKEN
-			return ['success' => false, 'msg' => '['.$datas['code'].'] '.$datas['message']];
-		} else {
-			// Done
-			return ['success' => true, 'data' => $datas];
-		}
-	}
+        $data = self::send('accounts/'.$accid, 'DELETE');
+        return $data;
+    }
+    
+    /**
+     * Retrieves the collection of Message resources.
+     */
+    public static function GetMessage(int $page = 1, ?string $token = null): array
+    {
+        self::$token = $token ?? self::$token;
 
+        return self::send('messages?page='.$page);
+    }
+
+    /**
+     * Retrieves a Message resource.
+     */
+    public static function GetMessageId(string $msgId, ?string $token = null) : array
+    {
+        self::$token = $token ?? self::$token;
+        return self::send('messages/'.$msgId);
+    }
+
+    /**
+     * Removes the Message resource.
+     */
+    public static function DeleteMessageId(string $msgId, ?string $token = null): array
+    {
+        self::$token = $token ?? self::$token;
+
+        return self::send('messages/'.$msgId, 'DELETE');
+    }
+
+    /**
+     * Retrieves a Account resource.
+     */
+    public static function Me(?string $token): array
+    {
+        self::$token = $token ?? self::$token;
+        return self::send('me');
+    }
 }
-
-?>
